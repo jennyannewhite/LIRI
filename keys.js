0b52f09d7650af76980432789a6ef065a4604225
jennyannewhite <30728980+jennyannewhite@users.noreply.github.com>
2018-02-05 22:03:34 -0600

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'lAHFOrQfUMf8mZBQ1Oumvka13',
+    consumer_secret: 'ULSRDPfXLNNu8MctV97vdnQEe9hqiG89geodkFDwU9QxfBIeTT',
+    access_token_key: '959978024849625088-Zfi5THOEK1CLJHDeVpmlEWrRjWYXpFZ',
+    access_token_secret: 'A8N5jDFGU2OD33R5gEMPERzaO9Gbp2VqDJhZ6MuWvV5Bb',
+}
+
+exports.spotifyKeys = {
+    client_ID: '438044242a1747e6af60f72026183ffc',
+    client_secret: '82d35dc9779b47958de24c6b0f9b6984',
+}
---
+++ liri.js
@@ -0,0 +1,183 @@
+require("dotenv").config();
+
+// defining the variables to grab the npm packages
+var twitter = require('twitter');
+var spotifyAPI = require('spotify-web-api-node');
+var request = require('request');
+var inquirer = require('inquirer');
+
+var fs = require('fs');
+
+// loading twitter and spotify keys information from keys.js file
+var keys = require('./keys.js');
+
+// loading twitter credentials
+var client = new twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+})
+
+// loading spotify credentials
+var spotify = new spotifyAPI({
+    clientId: keys.spotifyKeys.client_ID,
+    clientSecret: keys.spotifyKeys.client_secret,
+})
+
+// take in the arguments
+var commandline = '';
+for (var i = 2; i < process.argv.length; i++) {
+    var b = process.argv[i];
+    commandline += b + ' ';
+}
+var action = process.argv[2];
+var itemName = ''; //this is a string
+for (var i = 3; i < process.argv.length; i++) {
+    var a = process.argv[i];
+    itemName += a + ' ';
+}
+
+// FUNCTIONS===========================================
+// main command
+function mainCommand() {
+    fs.appendFile('log.txt', 'Command line: ' + commandline + '\n', function(err) {
+        if (err) return console.log('Error occured: ' + err);
+        console.log('This command line has been updated in log.txt.')
+        console.log("====================");
+    })
+    switch (action) {
+        case "my-tweets":
+            readTweets();
+            break;
+        case "spotify-this-song":
+            findSong();
+            break;
+        case "movie-this":
+            findMovie();
+            break;
+        case "do-what-it-says":
+            doIt();
+            break;
+    }
+}
+
+
+// handling twitter
+function readTweets() {
+
+    var params = { q: 'nodejs', count: 20 };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < 20; i++) {
+                console.log(i + 1 + ":   " + tweets[i].text);
+                console.log("Date added: " + tweets[i].created_at);
+                console.log("====================");
+            }
+
+        }
+    });
+
+}
+
+
+// handling spotify
+function findSong() {
+
+    var searchResults = [];
+
+    if (itemName) {
+        spotify.searchTracks(itemName, { limit: 5, offset: 0 }).then(function(data) {
+            for (var i = 0; i < 5; i++) {
+                searchResults.push(data.body.tracks.items[i].name + ' by ' + data.body.tracks.items[i].artists[0].name);
+            }
+            console.log("====================");
+            inquirer.prompt({
+                type: 'list',
+                name: 'songs',
+                message: "Here are the top 5 results. Which one do you want to know more about?",
+                choices: searchResults,
+
+            }).then(function(answer) {
+                for (var i = 0; i < 5; i++) {
+                    if (answer.songs.split(' by ')[0] == data.body.tracks.items[i].name && answer.songs.split(' by ')[1] == data.body.tracks.items[i].artists[0].name) {
+                        console.log("====================");
+                        console.log("Song name: " + data.body.tracks.items[i].name);
+                        console.log("Artist(s): " + data.body.tracks.items[i].artists[0].name);
+                        console.log("From album: " + data.body.tracks.items[i].album.name);
+                        console.log("Preview link: " + data.body.tracks.items[i].preview_url);
+                        console.log("====================");
+                    }
+                }
+            })
+
+        }, function(err) {
+            console.log('Error occurred: ' + err);
+
+        })
+
+    } else {
+        spotify.searchTracks('track:The Sign artist:Ace of Base').then(function(data) {
+            console.log("====================");
+            console.log("Song name: " + data.body.tracks.items[i].name);
+            console.log("Artist(s): " + data.body.tracks.items[i].artists[0].name);
+            console.log("From album: " + data.body.tracks.items[i].album.name);
+            console.log("Preview link: " + data.body.tracks.items[i].preview_url);
+            console.log("====================");
+        }, function(err) {
+            return console.log(err);
+        });
+    }
+
+}
+
+function findMovie() {
+
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + itemName + "&y=&plot=short&r=json";
+
+    function search() {
+
+        request(queryUrl, function(error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+
+
+                console.log("====================");
+                console.log("Movie title: " + JSON.parse(body).Title);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Release Year: " + JSON.parse(body).Year);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("====================");
+            }
+
+        });
+    }
+
+    if (itemName) {
+        search();
+
+    } else {
+        var itemName = 'Mr. Nobody'
+        var queryUrl = "http://www.omdbapi.com/?t=" + itemName + "&y=&plot=short&r=json";
+        search();
+    }
+
+}
+
+function doIt() {
+    fs.readFile('random.txt', 'utf8', function(err, data) {
+        if (err) return console.log(err);
+        console.log(data);
+        action = data.split(',')[0];
+        itemName = data.split(',')[1];
+        mainCommand();
+    })
+}
+
+// MAIN ACTION=========================================
+mainCommand();
